40730d15c650e50e76464429f3b6de5baa88f14d
Anand27git <anandkumar256k@gmail.com>
2021-07-11 21:22:22 +0530

    Address Book DataBase created

---
+++ Address_Book.sql
@@ -0,0 +1,27 @@
+/*******
+*
+*Welcome to Address_Book Using MySql Database
+*
+****/
+
+/* UC-1 creation of Address_Book Service */
+mysql> Create database Address_Book;
+Query OK, 1 row affected (0.35 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| address_book       |
+| classicmodels      |
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sys                |
++--------------------+
+7 rows in set (0.00 sec)
+
+mysql> USE Address_Book;
+Database changed
+mysql>
